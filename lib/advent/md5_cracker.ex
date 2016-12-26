2d3999d707029990734b1f4efb091dc7fd3cccfc
Peter Thompson <peter.thompson@dunelm.org.uk>
2016-12-26 18:47:57 +0100

    Day 5 - part 2

--- lib/advent/md5_cracker.ex
+++ lib/advent/md5_cracker.ex
@@ -1,6 +1,6 @@
 defmodule Advent.MD5Cracker do
   @doc """
-  Works out the password for the given door ID.
+  Works out the password for the given door ID using the rules for part 1 of day 5.
   
     iex> Advent.MD5Cracker.crack("abc")
     "18f47a30"
@@ -20,5 +20,61 @@ defmodule Advent.MD5Cracker do
       "00000" <> rest -> _crack(door_id, i + 1, password <> String.first(rest))
       _ -> _crack(door_id, i + 1, password)
     end
+  end
+  
+  @doc """
+  Works out the password for the given door ID using the rules for part 2 of day 5.
+  
+    iex> Advent.MD5Cracker.crack_with_pos("abc")
+    "05ace8e3"
+  """
+  def crack_with_pos(door_id) do
+    _crack_with_pos(door_id, 0, %{
+      "0" => "_",
+      "1" => "_",
+      "2" => "_",
+      "3" => "_",
+      "4" => "_",
+      "5" => "_",
+      "6" => "_",
+      "7" => "_",
+      count: 0
+    })
+  end
+  
+  defp _crack_with_pos(door_id, i, state)
+  defp _crack_with_pos(_, _, %{count: count} = pw) when count == 8, do: _pw_state_to_string(pw)
+  defp _crack_with_pos(door_id, i, state) do
+    hash = :crypto.hash(:md5, door_id <> Integer.to_string(i))
+    |> Base.encode16
+    
+    case hash do
+      "00000" <> rest -> _get_char(rest, door_id, i, state)
+        _ -> _crack_with_pos(door_id, i + 1, state)
+        
+    end
+  end
+  
+  # Checks if the first char of hash_tail is a valid position in the PW, and if yes, puts the 2nd
+  # char from hash_tail into the given position in the PW state.
+  defp _get_char(hash_tail, door_id, i, state) do
+    {pos, rest} = String.next_codepoint(hash_tail)
+    
+    case Map.has_key?(state, pos) and Map.get(state, pos) == "_" do
+      true -> _crack_with_pos(door_id, i + 1, _update_state(state, pos, String.first(rest)))
+      false -> _crack_with_pos(door_id, i + 1, state)
+    end
+  end
+  
+  # Sets the PW state char at `pos` to `pw_char` and increments the count of found characters.
+  defp _update_state(state, pos, pw_char) do
+    new_state = Map.put(%{ state | count: state.count + 1 }, pos, pw_char)
+    _pw_state_to_string(new_state) |> IO.puts
+    new_state
+  end
+  
+  defp _pw_state_to_string(pw) do
+    pw_string = pw["0"] <> pw["1"] <> pw["2"] <> pw["3"] <> pw["4"] <> pw["5"] <> pw["6"] <> pw["7"]
+    String.downcase(pw_string)
   end
 end
--- lib/day5.exs
+++ lib/day5.exs
@@ -1,3 +1,6 @@
 door_id = "abbhdwsy"
-password = Advent.MD5Cracker.crack(door_id)
-IO.puts "The password is #{password}"
+# password = Advent.MD5Cracker.crack(door_id)
+# IO.puts "The password is #{password}"
+
+second_password = Advent.MD5Cracker.crack_with_pos(door_id)
+IO.puts "The real password is #{second_password}"
